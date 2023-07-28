7627318efc3e5a1c54026308e98bd9dd991ec406
ldsenow <ldsenow@gmail.com>
2023-07-29 00:39:41 +1000

    Add project files.

---
+++ Client/Components/CustomSelectOption.cs
@@ -0,0 +1,20 @@
+﻿namespace AntDemoBlazorApp.Client.Components
+{
+    public class CustomSelectOption<TValue>
+    {
+        public TValue Value { get; set; }
+        public string Label { get; set; }
+
+        //public override bool Equals(object? obj)
+        //{
+        //    if (obj is CustomSelectOption<TValue> option)
+        //    {
+        //        return option.Value.Equals(Value);
+        //    }
+
+        //    return false;
+        //}
+
+        //public override int GetHashCode() => Value == null ? Value.GetHashCode();
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,16 @@
+using AntDemoBlazorApp.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("AntDemoBlazorApp.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("AntDemoBlazorApp.ServerAPI"));
+
+builder.Services.AddAntDesign();
+
+await builder.Build().RunAsync();
